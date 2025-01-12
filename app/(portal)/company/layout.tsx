66f70fff737220079d61ecd57321dd65a553169c
Kennedy Mwangi <kennedygithinjioffice@gmail.com>
2025-01-12 18:14:21 +0300

    23

--- app/(portal)/company/layout.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardLayout from '@/components/layout/DashboardLayout';
-
-export default function CompanyLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
